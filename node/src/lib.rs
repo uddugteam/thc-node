cd2aa8603f9f73920a88d320d8eac17d9409235a
Andrew Skurlatov <a.skurlatov@gmail.com>
2020-12-16 13:08:12 +0300

    wip: core pallets

--- node/src/lib.rs
+++ node/src/lib.rs
@@ -1,3 +1,15 @@
-pub mod chain_spec;
-pub mod service;
-pub mod rpc;
+//! RPC Node Template CLI library.
+#![warn(missing_docs)]
+#![warn(unused_extern_crates)]
+
+mod chain_spec;
+#[macro_use]
+mod service;
+mod cli;
+mod command;
+mod rpc;
+mod silly_rpc;
+
+fn main() -> sc_cli::Result<()> {
+    command::run()
+}
--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -1,103 +1,121 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-/// Edit this file to define custom logic or remove it if it is not needed.
-/// Learn more about FRAME and the core library of Substrate FRAME pallets:
-/// https://substrate.dev/docs/en/knowledgebase/runtime/frame
-
-use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch, traits::Get};
+use frame_support::{
+    codec::{Decode, Encode},
+    debug, decl_error, decl_event, decl_module, decl_storage, ensure, StorageMap,
+};
 use frame_system::ensure_signed;
+use sp_runtime::print;
+use sp_std::vec::Vec;
 
-#[cfg(test)]
-mod mock;
-
-#[cfg(test)]
-mod tests;
+#[derive(Encode, Decode, Default, Clone, PartialEq)]
+pub struct PersonData {
+    age: u32,
+    sex: u32,
+    bmi: u32,
+}
 
 /// Configure the pallet by specifying the parameters and types on which it depends.
 pub trait Trait: frame_system::Trait {
-	/// Because this pallet emits events, it depends on the runtime's definition of an event.
-	type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
+    /// Because this pallet emits events, it depends on the runtime's definition of an event.
+    type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
 }
 
 // The pallet's runtime storage items.
 // https://substrate.dev/docs/en/knowledgebase/runtime/storage
 decl_storage! {
-	// A unique name is used to ensure that the pallet's storage items are isolated.
-	// This name may be updated, but each pallet in the runtime must use a unique name.
-	// ---------------------------------vvvvvvvvvvvvvv
-	trait Store for Module<T: Trait> as TemplateModule {
-		// Learn more about declaring storage items:
-		// https://substrate.dev/docs/en/knowledgebase/runtime/storage#declaring-storage-items
-		Something get(fn something): Option<u32>;
-	}
+    trait Store for Module<T: Trait> as TemplateModule {
+        /// The storage item for our proofs.
+        /// It maps a proof to the user who made the claim and when they made it.
+        // Persons get(fn persons): map hasher(blake2_128_concat) T::AccountId => PersonData;
+        Persons get(fn persons): map hasher(blake2_128_concat) T::AccountId => (u32,u32,u32);
+
+        Thing1 get(fn thing1): u32;
+        Thing2 get(fn thing2): u32;
+    }
 }
 
 // Pallets use events to inform users when important changes are made.
+// Event documentation should end with an array that provides descriptive names for parameters.
 // https://substrate.dev/docs/en/knowledgebase/runtime/events
-decl_event!(
-	pub enum Event<T> where AccountId = <T as frame_system::Trait>::AccountId {
-		/// Event documentation should end with an array that provides descriptive names for event
-		/// parameters. [something, who]
-		SomethingStored(u32, AccountId),
-	}
-);
+decl_event! {
+    pub enum Event<T> where AccountId = <T as frame_system::Trait>::AccountId {
+        /// Event emitted when a person has been created. [who]
+        PersonCreated(AccountId),
+
+        /// Event emitted when a person has been updated. [who]
+        PersonUpdated(AccountId),
+
+        /// Set new value. [key] [value]
+        ValueSet(u32, u32),
+    }
+}
 
 // Errors inform users that something went wrong.
 decl_error! {
-	pub enum Error for Module<T: Trait> {
-		/// Error names should be descriptive.
-		NoneValue,
-		/// Errors should have helpful documentation associated with them.
-		StorageOverflow,
-	}
+    pub enum Error for Module<T: Trait> {
+        NotEnoughDataFields
+    }
 }
 
 // Dispatchable functions allows users to interact with the pallet and invoke state changes.
 // These functions materialize as "extrinsics", which are often compared to transactions.
 // Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 decl_module! {
-	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-		// Errors must be initialized if they are used by the pallet.
-		type Error = Error<T>;
+    pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+        // Errors must be initialized if they are used by the pallet.
+        type Error = Error<T>;
 
-		// Events must be initialized if they are used by the pallet.
-		fn deposit_event() = default;
+        // Events must be initialized if they are used by the pallet.
+        fn deposit_event() = default;
 
-		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
-		#[weight = 10_000 + T::DbWeight::get().writes(1)]
-		pub fn do_something(origin, something: u32) -> dispatch::DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://substrate.dev/docs/en/knowledgebase/runtime/origin
-			let who = ensure_signed(origin)?;
+        /// Allow a user to claim ownership of an unclaimed proof.
+        #[weight = 10_000]
+        fn create_person(origin, age: u32, sex: u32, bmi: u32) {
+            // Check that the extrinsic was signed and get the signer.
+            // This function will return an error if the extrinsic is not signed.
+            // https://substrate.dev/docs/en/knowledgebase/runtime/origin
+            let sender = ensure_signed(origin)?;
 
-			// Update storage.
-			Something::put(something);
+            debug::info!("Create person request from {:?}", sender);
 
-			// Emit an event.
-			Self::deposit_event(RawEvent::SomethingStored(something, who));
-			// Return a successful DispatchResult
-			Ok(())
-		}
+            /*let body = PersonData {
+                age,
+                sex,
+                bmi,
+            };*/
 
-		/// An example dispatchable that may throw a custom error.
-		#[weight = 10_000 + T::DbWeight::get().reads_writes(1,1)]
-		pub fn cause_error(origin) -> dispatch::DispatchResult {
-			let _who = ensure_signed(origin)?;
+             // Store the proof with the sender and block number.
+            Persons::<T>::insert(&sender,(age,sex,bmi));
 
-			// Read a value from storage.
-			match Something::get() {
-				// Return an error if the value has not been set.
-				None => Err(Error::<T>::NoneValue)?,
-				Some(old) => {
-					// Increment the value read from storage; will error in the event of overflow.
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					// Update the value in storage with the incremented result.
-					Something::put(new);
-					Ok(())
-				},
-			}
-		}
-	}
+            // Emit an event that the person was created.
+            Self::deposit_event(RawEvent::PersonCreated(sender));
+        }
+
+        /// Sets the first simple storage value
+        #[weight = 10_000]
+        pub fn set_thing_1(origin, val: u32) {
+            let _ = ensure_signed(origin)?;
+
+            Thing1::put(val);
+
+            Self::deposit_event(RawEvent::ValueSet(1, val));
+        }
+
+        /// Sets the second stored value
+        #[weight = 10_000]
+        pub fn set_thing_2(origin, val: u32) {
+            let _ = ensure_signed(origin)?;
+
+            Thing2::put(val);
+
+            Self::deposit_event(RawEvent::ValueSet(2, val));
+        }
+    }
+}
+
+impl<T: Trait> Module<T> {
+    pub fn get_sum() -> u32 {
+        Thing1::get() + Thing2::get()
+    }
 }
